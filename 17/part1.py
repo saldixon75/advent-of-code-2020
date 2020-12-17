e4add1f8cc4822073389c40b03703d91a2a4b985
sal dixon <sally.dixon@ft.com>
2020-12-17 21:01:53 +0000

    fix stupid typo bug

--- 17/part1.py
+++ 17/part1.py
@@ -1,5 +1,6 @@
 import numpy;
 import sys;
+import copy;
 
 with open("input.txt") as f:
     rows = f.read().splitlines();
@@ -30,11 +31,11 @@ def get_neighbours(location_key):
     neighbours.remove("{}:{}:{}".format(x,y,z));
     return neighbours;
 
-def get_number_of_active_neighbours(neighbours):
+def get_number_of_active_neighbours(neighbours, current_layout):
     running_total = 0;
     for n in neighbours:
-        if n in current_universe:
-            state = current_universe[n];
+        if n in current_layout:
+            state = current_layout[n];
         else:
             state = '.';    
         
@@ -44,32 +45,36 @@ def get_number_of_active_neighbours(neighbours):
 
 def get_next_state(current_state, number_of_active_neighbours):
     if (current_state == '#'):
-        if ((number_of_active_neighbours < 2)  or ( number_of_active_neighbours >3)):
+        if ((number_of_active_neighbours < 2)  or ( number_of_active_neighbours > 3 )):
             return '.';   #it dies
     else:
         if ( number_of_active_neighbours == 3 ):
             return '#'; # comes to life
     return current_state; # no change    
 
-def apply_cycle_rules(current_layout):
+def apply_cycle_rules(current_universe):
+    current_layout = copy.deepcopy(current_universe);
     next_layout = {};
     for cube_location in current_layout.keys():
         current_state = current_layout[cube_location];
+
         neighbours = get_neighbours(cube_location);
-        # print(neighbours);
-        number_of_active_neighbours = get_number_of_active_neighbours(neighbours);
-        # print("number_of_active_neighbours=" + str(number_of_active_neighbours));
+        number_of_active_neighbours = get_number_of_active_neighbours(neighbours, current_layout);
         next_state = get_next_state(current_state,number_of_active_neighbours);
         next_layout[cube_location] = next_state;
+
         #  now what about all those neighbours?  if they are already in the current universe, don't worry.
         #  if there are NOT currently mapped, we need to add them...
         for n in neighbours:
             if ((n in current_layout) or (n in next_layout)):
                 #  already taken care of. do nowt.
                 continue;
+
+            n_current_state = current_layout[n] if n in current_layout else '.';
+
             n_neighbours = get_neighbours(n);
-            number_of_active_n_neighbours = get_number_of_active_neighbours(n_neighbours);
-            next_state = get_next_state(current_state,number_of_active_n_neighbours);
+            number_of_active_n_neighbours = get_number_of_active_neighbours(n_neighbours, current_layout);
+            next_state = get_next_state(n_current_state,number_of_active_n_neighbours);
             next_layout[n] = next_state;
     return next_layout;
 
@@ -77,11 +82,12 @@ def get_total_active(layout):
     count = len(filter(lambda x: x == '#', layout.values()));
     return count;
 
-for i in range(1,6):    
+for i in range(1,7):    
+    # print('ABOUT TO DO ROUND ' + str(i) + ' number of active = ' + str(get_total_active(current_universe)));
     next_universe = apply_cycle_rules(current_universe  ) ;
     number_active = get_total_active(next_universe);
-    print("i= " + str(i) + " How many active? = " + str(number_active));
-    current_universe = next_universe.copy() ;
+    print("Round " + str(i) + " How many active now? = " + str(number_active));
+    current_universe = copy.deepcopy(next_universe) ;
 
 
 
--- 17/part2.py
+++ 17/part2.py
@@ -1,20 +1,21 @@
 import numpy;
 import sys;
+import copy;
 
 with open("input.txt") as f:
     rows = f.read().splitlines();
 
 gridsize = len(rows);
 #  start the co-ordinate system at the top left of the initial 2D grid provided
-current_layout = {};
+current_universe = {};
 
 for r in range(gridsize):
     cols = list(rows[r]);
     for c in range(len(cols)):
-        current_layout["{}:{}:0:0".format(c,r)] = cols[c];
+        current_universe["{}:{}:0:0".format(c,r)] = cols[c];
 
 print("Starting layout is ...");
-print(current_layout);
+print(current_universe);
 
 # def get_neighbours(x,y,z):
 def get_neighbours(location_key):
@@ -32,7 +33,7 @@ def get_neighbours(location_key):
     neighbours.remove("{}:{}:{}:{}".format(x,y,z,w));
     return neighbours;
 
-def get_number_of_active_neighbours(neighbours):
+def get_number_of_active_neighbours(neighbours, current_layout):
     running_total = 0;
     for n in neighbours:
         if n in current_layout:
@@ -46,32 +47,36 @@ def get_number_of_active_neighbours(neighbours):
 
 def get_next_state(current_state, number_of_active_neighbours):
     if (current_state == '#'):
-        if ((number_of_active_neighbours < 2)  or ( number_of_active_neighbours >3)):
+        if ((number_of_active_neighbours < 2)  or ( number_of_active_neighbours > 3 )):
             return '.';   #it dies
     else:
         if ( number_of_active_neighbours == 3 ):
             return '#'; # comes to life
     return current_state; # no change    
 
-def apply_cycle_rules(current_layout):
+def apply_cycle_rules(current_universe):
+    current_layout = current_universe;
     next_layout = {};
     for cube_location in current_layout.keys():
         current_state = current_layout[cube_location];
+
         neighbours = get_neighbours(cube_location);
-        # print('number of neighbours is .. ' + str(len(neighbours)));
-        number_of_active_neighbours = get_number_of_active_neighbours(neighbours);
-        # print("number_of_active_neighbours=" + str(number_of_active_neighbours));
+        number_of_active_neighbours = get_number_of_active_neighbours(neighbours, current_layout);
         next_state = get_next_state(current_state,number_of_active_neighbours);
         next_layout[cube_location] = next_state;
+
         #  now what about all those neighbours?  if they are already in the current universe, don't worry.
         #  if there are NOT currently mapped, we need to add them...
         for n in neighbours:
             if ((n in current_layout) or (n in next_layout)):
                 #  already taken care of. do nowt.
                 continue;
+
+            n_current_state = current_layout[n] if n in current_layout else '.';
+
             n_neighbours = get_neighbours(n);
-            number_of_active_n_neighbours = get_number_of_active_neighbours(n_neighbours);
-            next_state = get_next_state(current_state,number_of_active_n_neighbours);
+            number_of_active_n_neighbours = get_number_of_active_neighbours(n_neighbours, current_layout);
+            next_state = get_next_state(n_current_state,number_of_active_n_neighbours);
             next_layout[n] = next_state;
     return next_layout;
 
@@ -79,11 +84,11 @@ def get_total_active(layout):
     count = len(filter(lambda x: x == '#', layout.values()));
     return count;
 
-for i in range(6):    
-    next_layout = apply_cycle_rules(current_layout);
-    number_active = get_total_active(next_layout);
-    print("i= " + str(i) + " How many active? = " + str(number_active));
-    current_layout = next_layout.copy() ;
+for i in range(1,7):    
+    next_universe = apply_cycle_rules(current_universe  ) ;
+    number_active = get_total_active(next_universe);
+    print("Round " + str(i) + " How many active now? = " + str(number_active));
+    current_universe = next_universe ;
 
 
 
