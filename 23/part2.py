6eb72694b90bf063bcb46819565f26e87a0afb1e
sal dixon <sally.dixon@ft.com>
2020-12-31 15:33:15 +0000

    wip

--- 23/part2.py
+++ 23/part2.py
@@ -11,7 +11,7 @@ moves_to_do = 10000;
 # moves_to_do = 100;
 cupstring='389125467';
 
-cupstring='789465123';
+# cupstring='789465123';
 
 cups=list(map(int,list(cupstring)));
 #  add the extra cups
@@ -21,11 +21,15 @@ for i in range(len(cups)+1,number_of_cups+1):
 
 # print('cups are: ' + str(cups));
 print('Number of cups: ' + str(len(cups)))
+current_cup_pos = 0;
+current_cup = cups[0];
+print('Initial cup = ' + str(current_cup));
+# print('Initial cups = ' + str(cups));
 
 start = time.time();
 print("Started at " + str(time.gmtime(start)));
 
-
+# could memoize this if i convert the list argument into a string
 def get_destination(current_cup, exempt):
     candidate = current_cup;
     while ( candidate in exempt + [current_cup] ):
@@ -34,24 +38,26 @@ def get_destination(current_cup, exempt):
             candidate = number_of_cups; 
     return candidate;  
 
+
 def do_a_move():
     global cups;
     current_cup = cups.pop(0);
     # print('Current cup = ' + str(current_cup));
     #  get the next three cups
-    first_to_move = cups.pop(0);
-    second_to_move = cups.pop(0);
-    third_to_move = cups.pop(0);
+    three_to_move = cups[0:3];
+    cups[0:3] = []; # remove them
     # print('three_to_move = ' + str(three_to_move));
-    dest = get_destination(current_cup, [first_to_move, second_to_move, third_to_move]);
+    dest = get_destination(current_cup, three_to_move);
     # print('dest = ' + str(dest));
 
     #  now stick em in...
     destination_pos = cups.index(dest);
     # print('destination_pos = ' + str(destination_pos));
-    cups.insert(destination_pos+1,first_to_move);
-    cups.insert(destination_pos+2,second_to_move);
-    cups.insert(destination_pos+3,third_to_move);
+
+    # MUCH SLOWER!! cups = cups[0:destination_pos+1] + three_to_move + cups[destination_pos+1:];
+    cups.insert(destination_pos+1,three_to_move[0]);
+    cups.insert(destination_pos+2,three_to_move[1]);
+    cups.insert(destination_pos+3,three_to_move[2]);
 
     #  now move the current cup to the back
     cups.append(current_cup);
@@ -62,8 +68,8 @@ for i in range(moves_to_do):
     if i%1000 == 0:
         print('Playing move ' + str(i+1));
         print('time taken so far = ' + str((time.time()-start)/60) + ' minutes');
-    # cups = do_a_move(cups);
     do_a_move();
+    # (current_cup, current_cup_pos) = do_a_move(current_cup, current_cup_pos);
 
 
 # print('Now cups = ' + str(cups));
